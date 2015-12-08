2ff1d6d7a4926e1ee53d46775cc51dafb660b4d9
jchristy-lo <achristy@wellesley.edu>
2015-12-08 14:55:08 -0500

    some skeleton functions in js

--- project5.js
+++ project5.js
@@ -7,20 +7,81 @@ var GLOBAL = {
                "drugs": "cat", "education": "cat", "ethnicity": "cat", "height": "cont", 
                "income": "cont", "job": "cat", "offspring": "cat", "orientation": "cat", 
                "pets": "cat", "religion": "cat", "sex": "cat", "smokes": "cat", 
-               "speaks": "cat", "wordcount": "cont"} //tags CATegorical or CONTinuous variables
+               "speaks": "cat", "wordcount": "cont"}, //tags CATegorical or CONTinuous variables
+        ySelect: "",
+        xSelect: "",
+        tabbedData: {}
 }
 
 function run() {
-    d3.selectAll("svg")
-	    .append("text")
+    noSelected();
+	getDataRows(function(data) {
+	   GLOBAL.data = data;
+	   console.log(GLOBAL.data);
+	   getWordcount();
+    });
+}
+
+function tabOn (xvar, yvar){
+	console.log("XVAR is "+xvar);
+	GLOBAL.data.forEach(function (profile) {
+		if(profile.yvar in GLOBAL.tabbedData){
+			if(profile.xvar in GLOBAL.tabbedData.yvar){
+				GLOBAL.tabbedData.yvar.xvar =+ 1;
+			}else{
+				GLOBAL.tabbedData.yvar.xvar = 1;
+			}
+		}else{
+			GLOBAL.tabbedData.yvar = {};
+			GLOBAL.tabbedData.yvar.xvar =+ 1;
+		}
+    });
+
+    console.log(GLOBAL.tabbedData);
+}
+
+function scatterPlot (xvar, yvar){
+
+}
+
+function getWordcount(){
+	
+}
+
+function sortType (xvar , yvar){
+	tabOn(xvar, yvar);
+	if(GLOBAL.varTypes[xvar] === "cont" & GLOBAL.varTypes[yvar] === "cont"){
+		scatterPlot(xvar,yvar);
+	}
+}
+
+function noSelected (){
+	var viz = d3.select("#viz")
+	viz.append("text")
 		.attr("x",100)
 		.attr("y",150)
 		.attr("dy","0.35em")
 		.text("Pick two variables to see their relationship!");
+}
 
-	getDataRows(function(data) {
-	   GLOBAL.data = data;
-    });
+function updateX (selection){
+	selection = selection.value
+	GLOBAL.xSelect = selection;
+
+	if(selection==="" | GLOBAL.ySelect ===""){noSelected;}
+	else{
+		sortType(selection, GLOBAL.ySelect)
+	}
+}
+
+function updateY (selection){
+	selection = selection.value
+	GLOBAL.ySelect = selection;
+
+	if(selection==="" | GLOBAL.xSelect===""){noSelected;}
+	else{
+		sortType(GLOBAL.xSelect, selection)
+	}
 }
 
 function getDataRows (f) {
